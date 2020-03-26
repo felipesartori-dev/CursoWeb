d88f194caa009c20c3ea7ee6f19add364637c567
Felipe <felipe-sartori@outlook.com>
2020-03-26 19:29:05 -0300

    Acrescentado conteudo

---
+++ exercicios-js/fundamentos/comentario.js
@@ -0,0 +1,19 @@
+// Comentário de uma linha
+
+console.log('Linha 1')
+
+/*
+    Comentário de linhas múltiplas
+*/
+
+console.log('Linha 2')
+
+/*
+    Comentário de linhas múltiplas
+*/
+
+console.log('Linha 3')
+
+/*
+    Comentário de linhas múltiplas
+*/
--- exercicios-js/fundamentos/organizacao.js
+++ exercicios-js/fundamentos/organizacao.js
@@ -1,1 +1,2 @@
+console.log("Sentença de código")
 
