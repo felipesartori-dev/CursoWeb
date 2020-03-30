469a764ae8babf18d383d06cec4870f6ce2aa696
Felipe <felipe-sartori@outlook.com>
2020-03-30 16:05:23 -0300

    Criando objetos

---
+++ exercicios-js/fundamentos/objetos.js
@@ -0,0 +1,15 @@
+const prod1 = {}
+prod1.nome = 'Celular'
+prod1.codigo = 11458
+prod1.valor = 5890.97
+prod1 ["Testando atributo com espaço"] = 'Zé' //Evitar usar atributos com espaço
+
+console.log(prod1) // jeito1 de criar um objeto
+
+const prod2 = {
+    nome: 'TV',
+    codigo: 8784,
+    valor: 44758.58
+}
+
+console.log(prod2) // jeito2 de criar um objeto
